2612ceea35e6fa3b4c147d032a64505e7677ed64
Krushna Sinnarkar <krushnasinnarkar@gmail.com>
2024-06-09 06:07:35 +0530

    first commit

---
+++ src/constants/Technologies.jsx
@@ -0,0 +1,81 @@
+import { FaNodeJs } from "react-icons/fa";
+import { RiJavascriptLine, RiReactjsLine } from "react-icons/ri";
+import { SiMongodb, SiPhp } from "react-icons/si";
+import { motion } from "framer-motion"
+
+const iconVariants = (duration) => ({
+    initial: { y: -10 },
+    animate: {
+        y: [10, -10],
+        transition: {
+            duration: duration,
+            ease: "linear",
+            repeat: Infinity,
+            repeatType: "reverse"
+        },
+    },
+})
+
+const Technologies = () => {
+    return (
+        <div className="border-bborder-neutral-800 pb-4">
+            <motion.h1
+                whileInView={{ opacity: 1, y: 0 }}
+                initial={{ opacity: 0, y: -100 }}
+                transition={{ duration: 1 }}
+                className="my-20 text-center text-4xl"
+            >
+                Technologies
+            </motion.h1>
+            <motion.div
+                whileInView={{ opacity: 1, x: 0 }}
+                initial={{ opacity: 0, x: -100 }}
+                transition={{ duration: 1.5 }}
+                className="flex flex-wrap items-center justify-center gap-4"
+            >
+                <motion.div
+                    variants={iconVariants(2.5)}
+                    initial="initial"
+                    animate="animate"
+                    className="rounded-2xl border-4 border-neutral-800 p-4"
+                >
+                    <RiJavascriptLine className="text-7xl text-yellow-300" />
+                </motion.div>
+                <motion.div
+                    variants={iconVariants(3.5)}
+                    initial="initial"
+                    animate="animate"
+                    className="rounded-2xl border-4 border-neutral-800 p-4"
+                >
+                    <RiReactjsLine className="text-7xl text-cyan-400" />
+                </motion.div>
+                <motion.div
+                    variants={iconVariants(5)}
+                    initial="initial"
+                    animate="animate"
+                    className="rounded-2xl border-4 border-neutral-800 p-4"
+                >
+                    <FaNodeJs className="text-7xl text-green-500" />
+                </motion.div>
+                <motion.div
+                    variants={iconVariants(2)}
+                    initial="initial"
+                    animate="animate"
+                    className="rounded-2xl border-4 border-neutral-800 p-4"
+                >
+                    <SiPhp className="text-7xl text-sky-800" />
+                </motion.div>
+                <motion.div
+                    variants={iconVariants(6)}
+                    initial="initial"
+                    animate="animate"
+                    className="rounded-2xl border-4 border-neutral-800 p-4"
+                >
+                    <SiMongodb className="text-7xl text-green-500" />
+                </motion.div>
+            </motion.div>
+        </div>
+    )
+}
+
+export default Technologies
---
+++ src/constants/index.js
@@ -0,0 +1,69 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpg";
+
+export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. I have honed my skills in front-end technologies like React.js, as well as back-end technologies like PHP, Node.js, MySQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+
+export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. I have worked with a variety of technologies, including React.js, PHP, Node.js, MySQ, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies.`;
+
+export const EXPERIENCES = [
+  {
+    year: "Jul 23 - Aug 23",
+    role: "Data Analyst",
+    company: "MTE Pvt. Ltd.",
+    description: `Efficiently streamlined and optimized data workflows, enhancing overall data management processes. Displayed adeptness in data handling, utilizing advanced methods to extract, cleanse, and manipulate datasets. Utilized effective data extraction techniques to accurately identify trends, key patterns, and anomalies. Gained proficiency in data analytics tools, such as SQL, BI, MIRO, EXCEL, and relevant software applications `,
+    technologies: ["SQL", "BI", "Excel", "Miro"],
+  },
+  {
+    year: "Dec 22 - Jan 23",
+    role: "Data Analyst",
+    company: "Gullak Money",
+    description: `Growth Vertical: Achieved optimized retention funnel via data-driven insights, user research & SQL-Analysis. Optimized retention funnel, providing a strategic framework for 5 Lakh+, increasing retention by over 15%. Researched 25K user journeys to enhance user experience & engagement, led to a 10% churn rate reduction. Utilized primary research & SQL analysis of customer experiences to propose alternate revenue streams`,
+    technologies: ["SQL", "Excel", "User Research"],
+  },
+  {
+    year: "Apr 22 - Jun 22",
+    role: "Frontend Developer",
+    company: "Spreadit Infotech",
+    description: `Forging a New Path: Crafting a Revolutionary Webpage for Unprecedented User Engagement Enhancement. Devised innovative webpage funneling user data into specialized social groups, enabling personalized updates. Collaborated with the team on a comprehensive UX & UI refinement, driving a 25% upswing in user engagement. Utilized HTML, CSS, JS for webpage creation and SQL for data handling, ensuring a seamless user experience`,
+    technologies: ["HTML", "CSS", "Javascript", "SQL"],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "E-Commerce Website",
+    image: project1,
+    description:
+      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
+    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+  },
+  {
+    title: "Task Management App",
+    image: project2,
+    description:
+      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
+    technologies: ["HTML", "CSS", "Angular", "Firebase"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+  },
+  {
+    title: "Blogging Platform",
+    image: project4,
+    description:
+      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
+    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+  },
+];
+
+export const CONTACT = {
+  address: "767 Fifth Avenue, New York, NY 10153 ",
+  phoneNo: "+12 4555 666 00 ",
+  email: "me@example.com",
+};
