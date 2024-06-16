cb94464a010a7d238120e3fd9c2a2644b8cdc06a
Krushna Sinnarkar <78172473+krushnasinnarkar@users.noreply.github.com>
2024-06-16 06:03:11 +0530

    Update About.jsx

--- src/constants/About.jsx
+++ src/constants/About.jsx
@@ -14,28 +14,16 @@ const About = () => {
                 About
                 <span className="text-neutral-500"> Me</span>
             </motion.h1>
-            <div className="flex flex-wrap">
                 <motion.div
                     whileInView={{ opacity: 1, x: 0 }}
                     initial={{ opacity: 0, x: -100 }}
                     transition={{ duration: 0.5 }}
-                    className="w-full lg:w-1/2 lg:p-8"
-                >
-                    <div className="flex items-center justify-center">
-                        <img className="rounded-2xl w-2/5 lg:w-3/4" src={aboutImg} alt="about" />
-                    </div>
-                </motion.div>
-                <motion.div
-                    whileInView={{ opacity: 1, x: 0 }}
-                    initial={{ opacity: 0, x: 100 }}
-                    transition={{ duration: 0.5 }}
-                    className="w-full lg:w-1/2"
+                    className="w-full"
                 >
                     <div className="flex justify-center lg:justify-start">
                         <p className="my-2 max-w-xl py-6">{ABOUT_TEXT}</p>
                     </div>
                 </motion.div>
-            </div>
         </div>
     )
 }
