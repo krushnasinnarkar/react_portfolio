d255a05c936f0c1fb8e06fecc9e01460e44db1a0
Krushna Sinnarkar <78172473+krushnasinnarkar@users.noreply.github.com>
2024-06-16 08:04:23 +0530

    Update Navbar.jsx

--- src/constants/Navbar.jsx
+++ src/constants/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
                 <img ClasssName="mx-2 w-10" src={logo} alt="logo" />
             </div>
             <div className="m-8 flex item-center justify-center gap-4 text-2xl">
-                <a href="https://www.linkedin.com/in/krushna-sinnarkar-73b799234/">
+                <a href="https://www.linkedin.com/in/krushnasinnarkar">
                     <FaLinkedin />
                 </a>
                 <a href="https://github.com/krushnasinnarkar">
